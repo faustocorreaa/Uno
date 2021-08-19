9a4de55fc527e9b5f2f854f677b765cc5acf038f
Alpa204 <77805527+Alpa204@users.noreply.github.com>
2021-08-19 17:48:58 -0400

    Update PasswordValidator.java

--- src/ca/sheridancollege/project/PasswordValidator.java
+++ src/ca/sheridancollege/project/PasswordValidator.java
@@ -1,11 +1,23 @@
 package ca.sheridancollege.project;
+/*
+*
+*/
 
 public class PasswordValidator {
 
     /**
-     *
+     *The purpose of the class is to validate the password from the user input
+     *Password must have the 8 character length
+     *Password must contain at least one uppercase,one lower case,
+     *at least one number and one special character
+     *@modifiers: Fausto Correa, Kamaksh Dave, Ruchita Soni, Alpa Gandhi
+     *Date: 19 August 2021
      * @param pass
      */
+    
+    /*
+    *This method check the length of the password
+    */
     public Boolean checkLength(String pass) {
         // TODO - implement PasswordValidator.checkLength
         if (pass.length() >= 8) {
@@ -18,6 +30,10 @@ public class PasswordValidator {
      *
      * @param pass
      */
+    
+     /*
+    *This method check the strength of the password
+    */
     public Boolean checkStrong(String pass) {
         // TODO - implement PasswordValidator.checkStrong
         throw new UnsupportedOperationException();
