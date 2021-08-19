3f87feda0dcfc8da40471f60c2069b1be8bb2571
kamaksh38 <84790029+kamaksh38@users.noreply.github.com>
2021-08-19 17:45:17 -0400

    Update CardValue.java

--- src/ca/sheridancollege/project/CardValue.java
+++ src/ca/sheridancollege/project/CardValue.java
@@ -1,5 +1,10 @@
  package ca.sheridancollege.project;
-
+/**
+ * @modifier:  Fausto correa , Kamaksh Dave, Ruchita soni, Alpa Gandhi
+ * date : 19 August 2021
+ * Description: A enumerated class for cardValue  
+ */
+//enumerated class containing value of cards
 public enum CardValue {
 	ZERO,
 	ONE,
