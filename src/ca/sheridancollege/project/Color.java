a096d30e312b6aacac4b0deb313d9811a5466e4a
kamaksh38 <84790029+kamaksh38@users.noreply.github.com>
2021-08-19 17:49:27 -0400

    Update Color.java

--- src/ca/sheridancollege/project/Color.java
+++ src/ca/sheridancollege/project/Color.java
@@ -1,5 +1,11 @@
 package ca.sheridancollege.project;
 
+/**
+ * @author Fausto correa , Kamaksh Dave, Ruchita soni, Alpa Gandhi
+ * date : 19 August 2021
+ * Description: A enumerated class Color containing the colour cards
+ */
+//enumerated class 
 public enum Color {
 	BLUE,
 	RED,
